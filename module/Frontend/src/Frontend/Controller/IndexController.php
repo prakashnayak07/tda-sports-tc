9507286c06414452bf6e2ed823ad8fc5d8b483da
Digital <digital@thedevelopment.com.au>
2025-07-31 11:42:50 +0545

    404 page fixed

--- module/Frontend/src/Frontend/Controller/IndexController.php
+++ module/Frontend/src/Frontend/Controller/IndexController.php
@@ -32,4 +32,39 @@ class IndexController extends AbstractActionController
         return $viewModel;
     }
 
+    /**
+     * Test action to demonstrate 404 page
+     * Access via: /test-404
+     */
+    public function test404Action()
+    {
+        // This will trigger a 404 error
+        throw new \InvalidArgumentException('This is a test 404 page');
+    }
+
+    /**
+     * Test action to demonstrate custom 404 handling
+     * Access via: /not-found
+     */
+    public function notFoundAction()
+    {
+        // Simply return the 404 template
+        $viewModel = new ViewModel();
+        $viewModel->setTemplate('error/404');
+
+        return $viewModel;
+    }
+
+    /**
+     * Test action to demonstrate working 404 page
+     * Access via: /test-404-working
+     */
+    public function test404WorkingAction()
+    {
+        // This will show the 404 page without any route errors
+        $viewModel = new ViewModel();
+        $viewModel->setTemplate('error/404');
+
+        return $viewModel;
+    }
 }
